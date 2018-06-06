728f230047e22ca5c8a4290f13f08d98788371f3
Artem Musalitin <musalitin.a.a@gmail.com>
2018-06-06 19:20:51 +0400

    mod code in accordance with review

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,61 +2,41 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        max = array[0]
-
-        def findMax(value, max)
-          if value > max
-            max = value
-          end
-          return max
-        end
-
-        for i in 0..(array.length()-1)
-          max = findMax(array[i],max)
-        end
-
-        for i in 0..(array.length()-1)
+        for i in 0..(array.length-1)
           if array[i]>0
-            array[i] = max
+            array[i] = array.max
           end
         end
-
         array
       end
 
       def search(array, query)
-        right = array.length()-1
-        if right < 0
-          return -1
-        end
-        if right == 0
-          if array[right] == query
-            return right
+        return -1 if array.empty?
+        if array.length == 1
+          if array[0] == query
+            return 0
           else return -1
           end
         end
 
+        right = array.length-1
         left=0
 
         while right >= left
-
           if right == left
             if array[right] == query
               return right
             else return -1
             end
           end
-
           center = (right+left).div 2
-
           if array[center] == query
             return center
           elsif array[center] > query
             right = center - 1
           else
-            left = center +1
+            left = center + 1
           end
-
         end
       end
     end

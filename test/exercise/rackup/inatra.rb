15139f6241cff96eae64dd06293f9bcfc049d97f
Artem Musalitin <musalitin.a.a@gmail.com>
2018-06-06 22:59:10 +0400

    add solution inatra

--- test/exercise/rackup/inatra.rb
+++ test/exercise/rackup/inatra.rb
@@ -1,9 +1,18 @@
 module Inatra
   class << self
-    def routes
+    def get(path, &block)
+      @income = {}
+      @income[path] = block
+    end
+
+    def routes(&block)
+      instance_eval(&block)
     end
 
     def call(env)
+      path = env['PATH_INFO']
+      block = @income[path]
+      block.call(env)
     end
   end
 end

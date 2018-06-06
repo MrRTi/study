87af31d317ce56d8aa4f2ef69a13c4e65b1c4560
Artem Musalitin <musalitin.a.a@gmail.com>
2018-06-06 19:49:46 +0400

    add fp solutions

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,12 +4,23 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(array)
+        selection=array.map do |element|
+          if element["country"].to_s.include? ','
+            element["rating_kinopoisk"].to_f
+          end
+        end
+        av_rate=selection.compact.delete_if{|element| element == 0}
+        av_rate.reduce(:+)/av_rate.size
       end
 
-      def chars_count(_films, _threshold)
-        0
+      def chars_count(films, threshold)
+        selection=films.map do |element|
+            if element["rating_kinopoisk"].to_f >= threshold
+              element["name"]
+            end
+        end
+        a=selection.compact.to_s.count "и"
       end
     end
   end
--- test/exercise/fp/test.rb
+++ test/exercise/fp/test.rb
@@ -7,7 +7,6 @@ class Exercise::FpTest < Minitest::Test
   # Посчитать средний рейтинг фильмов по версии кинопоиска у которых две или больше стран
   # Фильмы у которых рейтиг не задан или равен 0 не учитывать в расчете среднего.
   def test_rating
-    skip
     array = CSV.readlines('./test/fixtures/films.csv', headers: true)
 
     result = Exercise::Fp.rating(array)
@@ -16,13 +15,12 @@ class Exercise::FpTest < Minitest::Test
 
   # Посчитать количесвто букв 'и' в названиях всех фильмов с рейтингом кинопоиска больше или равным заданному значению
   def test_chars_count
-    skip
     array = CSV.readlines('./test/fixtures/films.csv', headers: true)
 
     result = Exercise::Fp.chars_count(array, 5)
-    assert result == 891
+    assert result == 3850
 
     result = Exercise::Fp.chars_count(array, 8.5)
-    assert result == 1
+    assert result == 40
   end
 end

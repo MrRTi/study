7326600118d8555346ef5954e9e5031ff0a05102
Artem Musalitin <musalitin.a.a@gmail.com>
2018-06-12 13:29:17 +0400

    rubocop offenses fixes

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,10 +2,8 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        for i in 0..(array.length-1)
-          if array[i]>0
-            array[i] = array.max
-          end
+        for i in 0..(array.length - 1)
+          array[i] = array.max if array[i] > 0
         end
         array
       end
@@ -13,26 +11,21 @@ module Exercise
       def search(array, query)
         return -1 if array.empty?
         if array.length == 1
-          if array[0] == query
-            return 0
-          else return -1
-          end
+          return -1 unless array[0] == query
+          return 0
         end
 
-        right = array.length-1
-        left=0
+        right = array.length - 1
+        left = 0
 
         while right >= left
           if right == left
-            if array[right] == query
-              return right
-            else return -1
-            end
+            return -1 unless array[right] == query
+            return right
           end
-          center = (right+left).div 2
-          if array[center] == query
-            return center
-          elsif array[center] > query
+          center = (right + left).div 2
+          return center if array[center] == query
+          if array[center] > query
             right = center - 1
           else
             left = center + 1

ef5d4b5c92dd026832d05fc407a4beb3901b6ab9
Artem Musalitin <musalitin.a.a@gmail.com>
2018-06-12 13:35:56 +0400

    rubocop offenses fixes

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,22 +5,20 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(array)
-        selection=array.map do |element|
-          if element["country"].to_s.include? ','
-            element["rating_kinopoisk"].to_f
+        selection = array.map do |element|
+          if element['country'].to_s.include? ','
+            element['rating_kinopoisk'].to_f
           end
         end
-        av_rate=selection.compact.delete_if{|element| element == 0}
-        av_rate.reduce(:+)/av_rate.size
+        av_rate = selection.compact.delete_if { |element| element == 0 }
+        av_rate.reduce(:+) / av_rate.size
       end
 
       def chars_count(films, threshold)
-        selection=films.map do |element|
-            if element["rating_kinopoisk"].to_f >= threshold
-              element["name"]
-            end
+        selection = films.map do |element|
+          element['name'] if element['rating_kinopoisk'].to_f >= threshold
         end
-        a=selection.compact.to_s.count "и"
+        selection.compact.to_s.count 'и'
       end
     end
   end

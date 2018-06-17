0079a13c332b5ddde158ca067ea9cfc9540614f1
Artem Musalitin <musalitin.a.a@gmail.com>
2018-06-17 21:23:36 +0400

    inatra method nissing

--- test/exercise/rackup/inatra.rb
+++ test/exercise/rackup/inatra.rb
@@ -1,8 +1,15 @@
 module Inatra
   class << self
-    def get(path, &block)
-      @income = {}
-      @income[path] = block
+    def method_missing(method_name, *args, &block)
+      if method_name.to_s =~ /get/
+        @get_path ||= {}
+        @get_path[args.first.to_s] = block
+      elsif method_name.to_s =~ /post/
+        @post_path ||= {}
+        @post_path[args.first.to_s] = block
+      else
+        super
+      end
     end
 
     def routes(&block)
@@ -11,7 +18,14 @@ module Inatra
 
     def call(env)
       path = env['PATH_INFO']
-      block = @income[path]
+      if env['REQUEST_METHOD'] == 'GET'
+        block = @get_path[path]
+      elsif env['REQUEST_METHOD'] == 'POST'
+        block = @post_path[path]
+      else
+        puts 'ERROR REQUEST METHOD NOT FOUND'
+        return 'ERROR'
+      end
       block.call(env)
     end
   end
